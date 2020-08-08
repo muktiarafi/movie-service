496e929c62a8e7eb0cb9e76c798253136db92cfc
muktiarafi <arafizhaputra@gmail.com>
2020-08-08 10:22:45 +0700

    adding more routes

--- src/controller/movie_controller.py
+++ src/controller/movie_controller.py
@@ -10,3 +10,28 @@ class MoviesController(Resource):
     def get(self):
         movies = Movie.objects().to_json()
         return Response(movies, mimetype="application/json", status=200)
+
+    def post(self):
+        body = request.get_json()
+        movie = Movie(**body)
+        movie.version = 1
+        movie.save()
+        return Response(movie.to_json(), mimetype="application/json", status=201)
+
+class MovieController(Resource):
+    def put(self, id):
+        body = request.get_json()
+        movie = Movie.objects.get(id=id)
+        movie.name = body['name']
+        movie.genres = body['genres']
+        movie.casts = body['casts']
+        movie.version += 1
+        movie.save()
+
+        return Response(movie.to_json(), mimetype="application/json", status=200)
+
+    def delete(self, id):
+        movie = Movie.objects.get(id=id)
+        movie.delete()
+
+        return Response(movie.to_json(), mimetype="application/json", status=200)
--- src/controller/routes.py
+++ src/controller/routes.py
@@ -1,5 +1,6 @@
-from .movie_controller import MoviesController
+from .movie_controller import MoviesController, MovieController
 
 
 def initialize_routes(api):
-    api.add_resource(MoviesController, '/movies')
+    api.add_resource(MoviesController, '/api/movies')
+    api.add_resource(MovieController, '/api/movies/<id>')
