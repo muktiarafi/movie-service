4b3c03999b3edb420bed46db9a864eb96802990b
muktiarafi <arafizhaputra@gmail.com>
2020-08-10 10:35:44 +0700

    adding marshmallow for validation

--- src/controller/movie_controller.py
+++ src/controller/movie_controller.py
@@ -1,12 +1,13 @@
 import sys
 sys.path.append('..')
 
-from models.movie import Movie
+from models.movie import Movie, movie_schema
 from flask_restful import Resource
 from flask import Response, request
-from mongoengine.errors import FieldDoesNotExist, NotUniqueError, DoesNotExist, ValidationError, InvalidQueryError
-from errors.field_validation_error import FieldValidatonError
+from mongoengine.errors import FieldDoesNotExist, NotUniqueError, DoesNotExist, ValidationError
+from errors.field_validation_error import FieldValidationError
 from errors.not_found_error import NotFoundError
+from errors.duplicate_key_error import DuplicateKeyError
 
 class MoviesController(Resource):
     def get(self):
@@ -15,29 +16,38 @@ class MoviesController(Resource):
 
     def post(self):
         body = request.get_json()
-        try:
+        validate = movie_schema.validate(body)
+
+        if validate:
+            raise FieldValidationError(validate)
+
+        try:    
             movie = Movie(**body)
             movie.version = 1
             movie.save()
-        except ValidationError as ex:
-            raise FieldValidatonError(ValidationError.to_dict(ex))
+        except NotUniqueError as err:
+            raise DuplicateKeyError({ 'name': 'duplicate key error'})
 
         return Response(movie.to_json(), mimetype="application/json", status=201)
 
 class MovieController(Resource):
     def put(self, id):
         body = request.get_json()
+        validate = movie_schema.validate(body)
+
+        if validate:
+            raise FieldValidationError(validate)
+
         try:
             movie = Movie.objects.get(id=id)
-            movie.name = body['name']
-            movie.genres = body['genres']
-            movie.casts = body['casts']
-            movie.version += 1
-            movie.save()
-        except KeyError:
-            print('missing key')
         except DoesNotExist:
             raise NotFoundError()
+
+        movie.name = body['name']
+        movie.genres = body['genres']
+        movie.casts = body['casts']
+        movie.version += 1
+        movie.save()
 
         return Response(movie.to_json(), mimetype="application/json", status=200)
 
--- src/models/movie.py
+++ src/models/movie.py
@@ -1,9 +1,21 @@
+import sys
 import mongoengine_goodjson as gj
+from marshmallow import fields, Schema
+from marshmallow.validate import Length
 from .db import db
-
 
 class Movie(gj.Document):
     name = db.StringField(required=True, unique=True)
     casts = db.ListField(db.StringField(), required=True)
     genres = db.ListField(db.StringField(), required=True)
     version = db.IntField(min_value=0, default=1)
+
+class MovieSchema(Schema):
+    name = fields.Str(required=True)
+    casts = fields.List(fields.Str(), required=True)
+    genres = fields.List(fields.Str(), required=True)
+
+    class Meta:
+        fields = ('name', 'casts', 'genres')
+
+movie_schema = MovieSchema()
