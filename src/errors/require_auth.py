18b79d6896dcd4d2b45de0e5595364a24848c65c
muktiarafi <arafizhaputra@gmail.com>
2020-08-10 22:34:16 +0700

    normalizing auth error response

--- src/errors/require_auth.py
+++ src/errors/require_auth.py
@@ -2,4 +2,4 @@ from .custom_error import CustomError
 
 class RequireAuthError(CustomError):
     def __init__(self):
-        super().__init__(['access not permitted'], 400, 'Authentication Error')
+        super().__init__(['Not authorized'], 400, 'Unauthorized')
